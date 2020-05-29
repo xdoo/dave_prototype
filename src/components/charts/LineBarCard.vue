3cc42d0c23e88430a4c8d92d621aea901dc99120
alexander.kerscher <alexander.kerscher@muenchen.de>
2020-05-29 12:20:06 +0200

    fix text

--- src/components/charts/LineBarCard.vue
+++ src/components/charts/LineBarCard.vue
@@ -7,8 +7,8 @@
     <v-card-title>Vergleich der Belastung pro Zähldatum</v-card-title>
     <v-card-text>
       Die Grafik zeigt, wieviele Fahrzeuge, wecher Art, die Zählstelle an einem Datum passiert haben. Wenn Sie mit der
-      Maus über die Abschnitte fahren, werden Ihnen die genauen Zahlen angezeigt. Fahren Sie mit der Maus oben über die
-      Legende, so werden die Abschnitte hervorgehoben, die zur Farbe passen.
+      Maus über die Abschnitte fahren, werden Ihnen die genauen Zahlen angezeigt. Klicken Sie mit der Maus auf die Legende,
+      so werden die ausgewählten Abschnitte ausgeblendet.
     </v-card-text>
     <v-card-actions>
       <v-select
