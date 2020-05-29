04176e57600418608c3e18d40182177fe154c19d
xdoo <claus.straube@gmail.com>
2020-05-29 11:15:56 +0200

    countings...

--- src/views/ChartDemoView.vue
+++ src/views/ChartDemoView.vue
@@ -1,7 +1,50 @@
 <template>
   <v-container fluid class="pa-0">
     <v-row no-gutters>
-      <v-col cols="2">Zählungen</v-col>
+      <dummy-map></dummy-map>
+    </v-row>
+    <v-row>
+      <v-col cols="2">
+        <v-list width="100%" two-line subheader class="pt-10">
+          <v-subheader></v-subheader>
+          <v-list-item @click="dummyClick()">
+            <v-list-item-avatar>
+              <v-icon>mdi-map-marker-radius-outline</v-icon>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>Zählung vom 24.12.2019 </v-list-item-title>
+              <v-list-item-subtitle>Standardzählung</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="dummyClick()">
+            <v-list-item-avatar>
+              <v-icon>mdi-map-marker-radius-outline</v-icon>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>Zählung vom 16.04.2018 </v-list-item-title>
+              <v-list-item-subtitle>Querschnittzählung</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="dummyClick()">
+            <v-list-item-avatar>
+              <v-icon>mdi-map-marker-radius-outline</v-icon>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>Zählung vom 12.08.2015 </v-list-item-title>
+              <v-list-item-subtitle>Standardzählung</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="dummyClick()">
+            <v-list-item-avatar>
+              <v-icon>mdi-robot</v-icon>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>Sensorzählung</v-list-item-title>
+              <v-list-item-subtitle>---</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
       <v-col cols="10">
         <v-container class="pa-0">
           <v-row>
@@ -45,8 +88,9 @@
   import Vue from 'vue'
   import {Component} from "vue-property-decorator"
 
-  import HeatmapCard from "@/components/charts/HeatmapCard.vue"
+  import DummyMap from "@/components/map/DummyMap.vue"
 
+  import HeatmapCard from "@/components/charts/HeatmapCard.vue"
   import BelastungsplanCard from "@/components/charts/BelastungsplanCard.vue";
   import SteplineCard from "@/components/charts/StepLineCard.vue"
   import ZaehlstelleHeader from "@/components/zaehlstelle/ZaehlstelleHeader.vue"
@@ -57,8 +101,14 @@
     SteplineCard,
     ZaehlstelleHeader,
     BelastungsplanCard,
+    DummyMap
   }
 })
 export default class ChartDemoView extends Vue {
+
+  dummyClick() {
+    console.log("happy click :)")
+  }
+
 }
 </script>
